9efb5e824ccbdd067f1b1143361e7b191638710c
Ryota-Shiohara <ryotashiohara@keio.jp>
2026-04-24 13:41:46 +0900

    03A

---
+++ src/03/a/StringTest.java
@@ -0,0 +1,9 @@
+public class StringTest {
+    public static void main(String[] args) { 
+        int i = -4;  
+        System.out.println("Hello! " + i); 
+        System.out.println("Hello! " + i + 26); 
+        System.out.println("Hello! "+ (i + 26)); 
+    } 
+}
+
---
+++ src/03/a/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        int[] a = new int[26]; 
+        a[26] = 424; 
+    }
+}
